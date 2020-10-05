ef70de1a7ea1599f8f267b96934fb04e06e75230
slim254 <collinsokore2@gmail.com>
2020-10-05 09:21:57 +0300

    sorting arrays

---
+++ sort/sortinjs/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class test(models.Model):
+    Check=models.TextField()
---
+++ sort/sortinjs/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+# Create your views here.
+def createfirst(request):
+   return render(request,"sortinjs/first.html")
+
